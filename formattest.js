6d7320f585f836c4038866922842f716154b5047
Zain Khan <zain.mk@hotmail.com>
2025-02-12 14:32:40 -0700

    Add prettier pre push hook

---
+++ formattest.js
@@ -0,0 +1,20 @@
+// test line width
+const value = 123 || 123 || 123 || 123 || 123 || 123 || 123 || 123 || 123 || 123 || 123 || 123 || 123 || 123 
+
+// test trailing commna
+const obj = {
+  one: '1',
+  two: '2'
+}
+
+// test tab width
+const obj2 = {
+    one: '1',
+    two: '2'
+}
+
+// test semicolon
+const hello = 'goodbye'
+
+//test single quote
+const testString = "hello and goodbye"
