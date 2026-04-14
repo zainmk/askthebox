c60f7e3c33d274d7b663077420e031851e107581
zainmk <zain.mk@hotmail.com>
2026-04-14 11:31:27 -0600

    no separate lists

--- src/components/CardList.js
+++ src/components/CardList.js
@@ -18,37 +18,14 @@ function CardList({ isLoading }){
         
         <Cards type={<SearchCard />} />
 
-        <p style={{ color:'white' }}> R E Q U E S T E D </p>
-
         <hr style = {{ width: '100%', borderColor: 'white' }} />
-         {isLoading ? <CircularProgress /> : mediaList?.filter(entry => entry.status !== 'like')?.map((media) => (
+         {isLoading ? <CircularProgress /> : mediaList?.map((media) => (
             <Cards 
                 key={media.imdbID} 
                 onDelete={ ()=> setMediaList(mediaList?.filter((entry) => entry.imdbID !== media.imdbID)) } 
                 type={<MediaCard media={media} />}
             />
         ))}
-
-        <p style={{ color:'white' }}> A D D E D </p>
-
-        <hr style = {{ width: '100%', borderColor: 'white' }} />
-        {isLoading ? <CircularProgress /> : (
-        mediaList
-            ?.filter(entry => entry.status === 'like')
-            ?.sort((a, b) => a.Title?.localeCompare(b.Title))
-            ?.map((media) => (
-                <Cards 
-                    key={media.imdbID} 
-                    onDelete={() =>
-                    setMediaList(
-                        mediaList?.filter((entry) => entry.imdbID !== media.imdbID)
-                    )
-                    } 
-                    type={<MediaCard media={media} />}
-                />
-            ))
-        )}
-
     </Box>
     )
 }
